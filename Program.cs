c5123b091253a803ff6160f28d81a30df410e9cc
Sundar Krishnamurthy <sundar.krishnamurthy@concur.com>
2016-02-17 14:28:37 -0800

    First Checkin

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ADGetAllUsers {
+    using System.DirectoryServices;
+    using System.DirectoryServices.AccountManagement;
+
+    class Program {
+        static void Main(string[] args) {
+            // Construct context to query your Active Directory
+            using (var context = new PrincipalContext(ContextType.Domain, "[YOUR_DOMAIN]")) {
+                // Construct UserPrincipal object for this context
+                var userPrincipal = new UserPrincipal(context);
+                // Search and find every user in the system – PrincipalSearcher instance for what we need!
+                using (var searcher = new PrincipalSearcher(userPrincipal)) {
+                    var counter = 0u;
+                    // Iterate for all users in AD
+                    foreach (var result in searcher.FindAll()) {
+                        counter++;
+                        var de = result.GetUnderlyingObject() as DirectoryEntry;
+                        var samAccountName = de.Properties["samAccountName"].Value;
+                        System.Console.WriteLine("{0}: {1}", counter, samAccountName);
+                    }
+                }
+            }
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ADGetAllUsers
-Get all the users in your AD GAL!
+1.	VPN in to your corporate Windows Domain network
+2.	Bring up your git bash shell
+3.	Navigate to a folder where you want this application to run
+4.	Make a directory here called ADGetAllUsers – mkdir ADGetAllUsers
+5.	Issue a git clone https://github.com/p8cakes/ADGetAllUsers.git command
+6.	Change directory to ADGetAllUsers – cd ADGetAllUsers
+7.	Do an ls here to verify that files have indeed been retrieved
+8.	Open ADGetAllUsers.csproj in Visual Studio Express 2013 for Windows Desktop (or the installed version of Visual Studio that you have)
+9.	Edit Program.cs to replace "[YOUR_DOMAIN]" with your actual corporate domain name.
+10.	Compile and run your program!
